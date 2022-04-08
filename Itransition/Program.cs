c51ec8e11e59b5be9d443fa253c891569f7d11ef
tempous <tsvetkovigor5530@gmail.com>
2022-04-08 03:24:31 +0300

    Compute SHA3-256 of each binary file

--- Itransition/Program.cs
+++ Itransition/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SHA3.Net;
+using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Itransition
@@ -7,8 +9,16 @@ namespace Itransition
     {
         static void Main(string[] args)
         {
-            foreach (var file in Directory.EnumerateFiles("R:\\task2"))
-                Console.WriteLine(file);
+            var hashes = new List<string>();
+
+            using (var sha3256 = Sha3.Sha3256())
+            {
+                foreach (var file in Directory.EnumerateFiles("R:\\task2"))
+                {
+                    Console.WriteLine(file);
+                    Console.WriteLine(BitConverter.ToString(sha3256.ComputeHash(File.ReadAllBytes(file))));
+                }
+            }
         }
     }
 }
