089d0393ccccfe7891e1a9a2bc9108bc48b9751a
tempous <tsvetkovigor5530@gmail.com>
2022-04-08 03:57:35 +0300

    Code refactoring

--- SHA3-256/Program.cs
+++ SHA3-256/Program.cs
@@ -8,41 +8,29 @@ namespace Itransition
 {
     class Program
     {
-        static string GetHash(Sha3 sha3256, byte[] bytes)
-        {
-            return BitConverter.ToString(sha3256.ComputeHash(bytes)).Replace("-", "").ToLower();
-        }
+        static string GetHash(Sha3 sha3256, byte[] bytes) => BitConverter.ToString(sha3256.ComputeHash(bytes)).Replace("-", "").ToLower();
 
         static void Main(string[] args)
         {
             var hashes = new List<string>();
             var email = "test@gmail.com";
-            var result = "";
 
             using (var sha3256 = Sha3.Sha3256())
             {
                 foreach (var file in Directory.EnumerateFiles("R:\\task2"))
-                {
                     hashes.Add(GetHash(sha3256, File.ReadAllBytes(file)));
-                }
-
+                
                 Console.WriteLine("Generated hashes: " + hashes.Count);
                 foreach (var hash in hashes)
                     Console.WriteLine(hash);
 
-                Console.WriteLine();
-                Console.WriteLine("Sorted hashes: " + hashes.Count);
                 hashes.Sort();
+                Console.WriteLine("\nSorted hashes: " + hashes.Count);
                 foreach (var hash in hashes)
-                {
                     Console.WriteLine(hash);
-                    result += hash;
-                }
 
-                result += email;
-
-                Console.WriteLine();
-                Console.WriteLine("Result hash:");
+                string result = string.Join("", hashes) + email;
+                Console.WriteLine("\nResult hash:");
                 Console.WriteLine(GetHash(sha3256, Encoding.UTF8.GetBytes(result)));
             }
         }
