961316629b19d310d53fa03c00fcfa3a9becc577
tempous <tsvetkovigor5530@gmail.com>
2022-04-08 03:14:46 +0300

    Read names of binary files

--- Itransition/Program.cs
+++ Itransition/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Itransition
 {
@@ -6,7 +7,8 @@ namespace Itransition
     {
         static void Main(string[] args)
         {
-
+            foreach (var file in Directory.EnumerateFiles("R:\\task2"))
+                Console.WriteLine(file);
         }
     }
 }
