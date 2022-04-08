f71a2225bce607af685c561ad3a984540536da62
tempous <tsvetkovigor5530@gmail.com>
2022-04-09 02:58:22 +0300

    Read console arguments

---
+++ HMAC/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace HMAC
+{
+    class Program
+    {
+        static void Main(string[] arguments)
+        {
+            var args = arguments.Distinct();
+            var argCount = args.Count();
+
+            if (argCount < 3 || argCount % 2 == 0)
+                Console.WriteLine("Incorrect number of arguments - less than 3 or even!");
+        }
+    }
+}
