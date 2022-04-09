e390242c1783987d74cc241ede0b6d9244514849
tempous <tsvetkovigor5530@gmail.com>
2022-04-09 18:57:51 +0300

    Method to generate cryptographic strong random bytes

---
+++ HMAC/Generator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace HMAC
+{
+    class Generator
+    {
+        const byte SIZE = 256 / 8;
+
+        public static string GenerateKey()
+        {
+            var key = new byte[SIZE];
+            var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(key);
+            return BitConverter.ToString(key).Replace("-", "");
+        }
+    }
+}
