0ae618cec38cc38ce3ca89430d6f49e638c848c4
tempous <tsvetkovigor5530@gmail.com>
2022-04-10 23:03:38 +0300

    Create a structure to save winning moves

---
+++ HMAC/Rules.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HMAC
+{
+    class Rules
+    {
+        public static Dictionary<int, List<int>> Generate(string[] moves)
+        {
+            Dictionary<int, List<int>> rules = new();
+
+            var moveCount = moves.Length;
+            var offset = (moveCount- 1) / 2;
+
+            for (int i = 0; i < moveCount; i++)
+            {
+                var winPositions = new List<int>();
+
+                for (int j = i + 1; j <= i + offset; j++)
+                    winPositions.Add(j % moveCount);
+
+                rules.Add(i, winPositions);
+            }
+
+            return rules;
+        }
+    }
+}
