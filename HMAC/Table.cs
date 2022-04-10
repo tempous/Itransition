4cc21140c94023a8b26dfdea93372350dc422a06
tempous <tsvetkovigor5530@gmail.com>
2022-04-10 23:10:13 +0300

    Method to set the status of moves

---
+++ HMAC/Table.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HMAC
+{
+    class Table
+    {
+        public static string[,] ruleTable;
+        static int size;
+
+        static string GetStatus(Dictionary<int, List<int>> rules, int i, int j)
+        {
+            return rules[i].Contains(j) ? "Win" : "Lose";
+        }
+    }
+}
